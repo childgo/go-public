54b0b30ed95a244b81ba08c6ed8061ddf716857e
childgo <65421844+childgo@users.noreply.github.com>
2021-05-01 19:23:16 -0400

    Create innode.sh

---
+++ centos/innode/innode.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# ^ that line is important
+clear
+
+
+  echo "Press [CTRL+C] to stop.."
+	echo "Server Ip:" $(hostname -i)
+	echo "Server Host:" $(hostname -f)
+	usr=$1
+	cd $usr
+	echo "Detailed Inode usage: $(pwd)" ; for d in `find -maxdepth 1 -type d |cut -d\/ -f2 |grep -xv . |sort`; do c=$(find $d |wc -l) ; printf "$c\t\t- $d\n" ; done ; printf "Total: \t\t$(find $(pwd) | wc -l)\n"
+	echo "Total Size:" $(du -h $usr --max-depth=0)
+	echo "======================="
+	echo "======================="
+	#end command
