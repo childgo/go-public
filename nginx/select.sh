7a47418796ae3d84342f1a8401cedea484f48628
childgo <65421844+childgo@users.noreply.github.com>
2021-09-18 00:29:01 -0400

    Update select.sh

--- nginx/select.sh
+++ nginx/select.sh
@@ -1,6 +1,6 @@
 clear
 PS3='Please enter your choice: '
-options=("Total IP connected to Server 1" "List of IP connected to Server 2" "Lock Folder 3" "Unlock Folder 4" "check Lock 5" "Restart Nginx 6" "Reload Nginx 7" "nginx -t 8"  "Tail Nginx error Log 9" "Tail Nginx access Log 10" "Restart PHP 11" "Nginx conf Path 12" "Nginx Log Path 13" "disk space used and available 14" "pgrep -x inotify.alsco 15" "Enable SELinux + PHP + Apache to write/access php file 16" "check Nginx version 17" "Check ALSCO IP Firewall Setting 18" "CSF Firewall Setting 19" "Check Rate Limit  Excess 20" "Quit")
+options=("Total IP connected to Server 1" "List of IP connected to Server 2" "Lock Folder 3" "Unlock Folder 4" "check Lock 5" "Restart Nginx 6" "Reload Nginx 7" "nginx -t 8"  "Tail Nginx error Log 9" "Tail Nginx access Log 10" "Restart PHP 11" "Nginx conf Path 12" "Nginx Log Path 13" "disk space used and available 14" "pgrep -x inotify.alsco 15" "Enable SELinux + PHP + Apache to write/access php file 16" "check Nginx version 17" "Check ALSCO IP Firewall Setting 18" "CSF Firewall Setting 19" "Check Rate Limit  Excess 20" "clear the contents of ratelimit_tempIP_block 21" "Quit")
 select opt in "${options[@]}"
 do
 case $opt in
@@ -156,6 +156,15 @@ echo ""
 echo ""
 grep --color=always -r "limiting requests" /var/log/nginx/error.log
 ;;
+########################################################
+
+"clear the contents of ratelimit_tempIP_block 21")
+clear
+echo "truncate -s 0 /etc/nginx/conf.d/alsco_data_cookie_and_ip/ALSCO-Setting/.ratelimit_tempIP_block.alsco"
+truncate -s 0 /etc/nginx/conf.d/alsco_data_cookie_and_ip/ALSCO-Setting/.ratelimit_tempIP_block.alsco
+
+;;
+
 ########################################################
 
 
