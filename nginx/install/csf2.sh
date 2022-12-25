231836da1b245bc5db0458bfd14650ea50599f8f
childgo <65421844+childgo@users.noreply.github.com>
2022-12-24 22:01:01 -0500

    Update csf2.sh

--- nginx/install/csf2.sh
+++ nginx/install/csf2.sh
@@ -1,9 +1,3 @@
-systemctl disable firewalld;sleep 3;
-systemctl stop firewalld;sleep 3;
-yum clean all;sleep 3;
-yum -y update;sleep 3;
-yum -y install wget perl ipset unzip net-tools perl-libwww-perl;sleep 3;
-yum -y install perl-LWP-Protocol-https perl-GDGraph bind-utils;sleep 3;
 cd /opt;sleep 3;
 wget https://download.configserver.com/csf.tgz;sleep 3;
 tar -xzf csf.tgz;sleep 3;
