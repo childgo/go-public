3f566f488935c14298268d25e907136e4ca13aac
childgo <65421844+childgo@users.noreply.github.com>
2023-02-25 15:40:37 -0500

    Update select.sh

--- nginx/select.sh
+++ nginx/select.sh
@@ -161,28 +161,30 @@ echo "Done"
 echo "nginx -v"
 nginx -v
 echo ""
-echo "=======================e"
+echo "======================="
 echo "nginx -V"
 nginx -V
 printf '\n\n\n'
 
-echo "=======================e"
+echo "======================="
 echo "Below is Available Nginx Module"
 echo "yum list available | grep nginx-module"
 yum list available | grep nginx-module
 echo "=======================e"
 
 printf '\n\n\n'
-echo "=======================e"
+echo "======================="
 echo "Below is Installed Nginx Module"
 echo "yum list installed | grep nginx"
 yum list installed | grep nginx
-echo "=======================e"
+echo "======================="
 
 printf '\n\n\n'
 
-
-
+echo "======================="
+echo "nginx -v | Get Nginx Vertion"
+nginx -v
+echo "======================="
 
 ;;
 ########################################################
