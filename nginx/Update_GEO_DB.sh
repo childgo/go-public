c07eccc01505fa816ee18c7bff0cf1a20374ade9
childgo <65421844+childgo@users.noreply.github.com>
2023-03-26 22:35:34 -0400

    Update Update_GEO_DB.sh

--- nginx/Update_GEO_DB.sh
+++ nginx/Update_GEO_DB.sh
@@ -13,27 +13,8 @@ bold=$(tput bold)
 #####################################################
 #####################################################
 
-
-
-
-Mypath_php_script="/var/www/html/verify/ASN-Maxmind/DataBase/"
-
-cd $Mypath_php_script
-echo "${RED}${bold}Current Path " $(pwd)${NC}
-echo "#########################################33"
-echo "#########################################33"
-
-echo ""
-
-#Unlock Path
-chattr -R -i /var/www/html/
-
-
-Myfile1_phpPath="/var/www/html/verify/ASN-Maxmind/DataBase/GeoLite2-City.mmdb"
-Myfile2_phpPath="/var/www/html/verify/ASN-Maxmind/DataBase/GeoLite2-ASN.mmdb"
-Myfile3_phpPath="/var/www/html/verify/ASN-Maxmind/DataBase/GeoLite2-Country.mmdb"
-Myfile4_phpPath="/var/www/html/verify/ASN-Maxmind/DataBase/GeoIP2-ISP.mmdb"
-
+cd /usr/share/GeoIP/
+pwd
 
 
 wget https://alscoip.com/GEO_Maxmind_Database/GeoLite2-City.mmdb -O new-GeoLite2-City.mmdb
@@ -46,10 +27,19 @@ wget https://alscoip.com/GEO_Maxmind_Database/GeoIP2-ISP.mmdb -O new-GeoIP2-ISP.
 
 
 #Get Size for each file
-Size_GeoLite2_City=$(wc -c "/var/www/html/verify/ASN-Maxmind/DataBase/new-GeoLite2-City.mmdb" | awk '{print $1}')
-Size_GeoLite2_ASN=$(wc -c "/var/www/html/verify/ASN-Maxmind/DataBase/new-GeoLite2-ASN.mmdb" | awk '{print $1}')
-Size_GeoLite_Country=$(wc -c "/var/www/html/verify/ASN-Maxmind/DataBase/new-GeoLite2-Country.mmdb" | awk '{print $1}')
-Size_GeoIP_ISP=$(wc -c "/var/www/html/verify/ASN-Maxmind/DataBase/new-GeoIP2-ISP.mmdb" | awk '{print $1}')
+Size_GeoLite2_City=$(wc -c "/usr/share/GeoIP/new-GeoLite2-City.mmdb" | awk '{print $1}')
+Size_GeoLite2_ASN=$(wc -c "/usr/share/GeoIP/new-GeoLite2-ASN.mmdb" | awk '{print $1}')
+Size_GeoLite_Country=$(wc -c "/usr/share/GeoIP/new-GeoLite2-Country.mmdb" | awk '{print $1}')
+Size_GeoIP_ISP=$(wc -c "/usr/share/GeoIP/new-GeoIP2-ISP.mmdb" | awk '{print $1}')
+
+
+
+
+
+Myfile1_Path="/usr/share/GeoIP/GeoLite2-City.mmdb"
+Myfile2_Path="/usr/share/GeoIP/GeoLite2-ASN.mmdb"
+Myfile3_Path="/usr/share/GeoIP/GeoLite2-Country.mmdb"
+Myfile4_Path="/usr/share/GeoIP/GeoIP2-ISP.mmdb"
 
 
 
@@ -102,10 +92,10 @@ fi
 echo ""
 echo ""
 #Start Delete Files
-rm $Myfile1_phpPath
-rm $Myfile2_phpPath
-rm $Myfile3_phpPath
-rm $Myfile4_phpPath
+rm $Myfile1_Path
+rm $Myfile2_Path
+rm $Myfile3_Path
+rm $Myfile4_Path
 
 
 ################################################
@@ -119,10 +109,10 @@ mv new-GeoIP2-ISP.mmdb GeoIP2-ISP.mmdb
 
 
 
-echo "${RED}${bold}$Myfile1_phpPath" $(date -r $Myfile1_phpPath)${NC}
-echo "${RED}${bold}$Myfile2_phpPath" $(date -r $Myfile2_phpPath)${NC}
-echo "${RED}${bold}$Myfile3_phpPath" $(date -r $Myfile3_phpPath)${NC}
-echo "${RED}${bold}$Myfile4_phpPath" $(date -r $Myfile4_phpPath)${NC}
+echo "${RED}${bold}$Myfile1_Path" $(date -r $Myfile1_Path)${NC}
+echo "${RED}${bold}$Myfile2_Path" $(date -r $Myfile2_Path)${NC}
+echo "${RED}${bold}$Myfile3_Path" $(date -r $Myfile3_Path)${NC}
+echo "${RED}${bold}$Myfile4_Path" $(date -r $Myfile4_Path)${NC}
 
 
 echo "#########################################"
@@ -131,74 +121,12 @@ echo "#########################################"
 
 
 
-#lock Path
-chattr -R +i /var/www/html/
-
-echo "lsattr /var/www/html/"
-lsattr /var/www/html/
 
 echo "Done"
 ############################################################################################################
 ############################################################################################################
 
 
-
-
-
-
-
-
-
-
-#Part 2 repalce files in /usr/share/GeoIP/
-echo "${GREEN}${bold}#########################################${NC}"
-echo "${GREEN}${bold}Part2${NC}"
-echo "${GREEN}${bold}#########################################${NC}"
-
-
-
-
-Mypath_Nginx_script="/usr/share/GeoIP/"
-cd $Mypath_Nginx_script
-echo "${RED}${bold}Current Path " $(pwd)${NC}
-echo "#########################################"
-
-
-
-
-Myfile1_Nginx="/usr/share/GeoIP/GeoLite2-ASN.mmdb"
-Myfile2_Nginx="/usr/share/GeoIP/GeoLite2-Country.mmdb"
-
-
-
-wget https://alscoip.com/GEO_Maxmind_Database/GeoLite2-Country.mmdb -O new-GeoLite2-Country.mmdb
-wget https://alscoip.com/GEO_Maxmind_Database/GeoLite2-ASN.mmdb -O new-GeoLite2-ASN.mmdb
-
-
-
-
-rm $Myfile1_Nginx
-rm $Myfile2_Nginx
-
-
-mv new-GeoLite2-Country.mmdb GeoLite2-Country.mmdb
-mv new-GeoLite2-ASN.mmdb GeoLite2-ASN.mmdb
-
-
-
-
-echo "${RED}${bold}$Myfile1_Nginx" $(date -r $Myfile1_Nginx)${NC}
-echo "${RED}${bold}$Myfile2_Nginx" $(date -r $Myfile2_Nginx)${NC}
-
-
-
-
-
-
-echo "#########################################"
-echo "#########################################"
-echo "#########################################"
-echo "#########################################"
 
 
 
