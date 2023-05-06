a20237b0453237f201caa68f44a3bc7671f3e9d1
childgo <65421844+childgo@users.noreply.github.com>
2023-05-06 00:47:46 -0400

    Update test.sh

--- Micro_Monitor/test.sh
+++ Micro_Monitor/test.sh
@@ -1,1 +1,8 @@
 
+#!/bin/bash
+
+# Change the root password to "hello@alsco"
+echo -e "hello@alsco\nhello@alsco" | passwd root
+
+# Print a message indicating that the password has been changed
+echo "The root password has been changed to: hello@alsco"
