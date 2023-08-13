9a080ff6c76df38024986e6189c4051e586112ef
childgo <65421844+childgo@users.noreply.github.com>
2023-08-12 22:10:03 -0400

    Update csf.sh

--- AlmaLinux9/install/csf.sh
+++ AlmaLinux9/install/csf.sh
@@ -2,7 +2,7 @@ systemctl disable firewalld;sleep 3;
 systemctl stop firewalld;sleep 3;
 yum clean all;sleep 3;
 yum -y update;sleep 3;
-yum -y install wget nano inotify-tools perl ipset unzip net-tools perl-libwww-perl;sleep 3;
+yum -y install wget nano inotify-tools perl tar ipset unzip net-tools perl-libwww-perl;sleep 3;
 yum -y install perl-LWP-Protocol-https perl-GDGraph bind-utils;sleep 3;
 cd /opt;sleep 3;
 wget https://download.configserver.com/csf.tgz;sleep 3;
