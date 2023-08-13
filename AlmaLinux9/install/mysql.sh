afcb5d153d27fa2ef45114ea43117428951abfdb
childgo <65421844+childgo@users.noreply.github.com>
2023-08-13 01:31:31 -0400

    Create mysql.sh

---
+++ AlmaLinux9/install/mysql.sh
@@ -0,0 +1,23 @@
+yum -y install mariadb-server mariadb;sleep 3;
+systemctl start mariadb;sleep 3;
+systemctl enable mariadb;sleep 3;
+#systemctl status mariadb;sleep 3;
+#mysql;sleep 3;
+
+
+#Allow Mysql
+
+
+sestatus;sleep 3;
+getsebool -a | grep httpd;sleep 3;
+setsebool httpd_can_network_connect_db 1;sleep 3;
+setsebool -P httpd_can_network_connect_db 1;sleep 3;
+setsebool -P httpd_can_network_connect 1;sleep 3;
+getsebool -a | grep httpd_can_network_connect;sleep 3;
+
+
+
+
+#checking
+systemctl status mariadb;sleep 3;
+mysql;sleep 3;
