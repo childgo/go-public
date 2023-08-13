8e58784e0982853119fa70f56a266f976c18e5c6
childgo <65421844+childgo@users.noreply.github.com>
2023-08-12 22:25:32 -0400

    Update csf.sh

--- AlmaLinux9/install/csf.sh
+++ AlmaLinux9/install/csf.sh
@@ -40,11 +40,10 @@ sed -i 's/UDP_IN =.*/UDP_IN ="53"/' /etc/csf/csf.conf
 
 
 
-sed -i 's/TCP6_IN = "20,21,22,25,53,80,110,143,443,465,587,993,995"/TCP6_IN = ""/' /etc/csf/csf.conf
-sed -i 's/TCP6_OUT = "20,21,22,25,53,80,110,113,443,587,993,995"/TCP6_OUT = ""/' /etc/csf/csf.conf
-
-sed -i 's/UDP6_IN = "20,21,53,80,443"/UDP6_IN = ""/' /etc/csf/csf.conf
-sed -i 's/UDP6_OUT = "20,21,53,113,123"/UDP6_OUT = ""/' /etc/csf/csf.conf
+sed -i 's/TCP6_IN =.*/TCP6_IN =""/' /etc/csf/csf.conf
+sed -i 's/TCP6_OUT =.*/TCP6_OUT =""/' /etc/csf/csf.conf
+sed -i 's/UDP6_IN =.*/UDP6_IN =""/' /etc/csf/csf.conf
+sed -i 's/UDP6_OUT .*/UDP6_OUT =""/' /etc/csf/csf.conf
 
 
 echo "Verify that the change was successful. ...."
