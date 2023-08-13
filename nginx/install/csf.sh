f08d76b28b5ad32de4f4fa4933052b29dcae71e9
childgo <65421844+childgo@users.noreply.github.com>
2023-08-12 22:39:49 -0400

    Update csf.sh

--- nginx/install/csf.sh
+++ nginx/install/csf.sh
@@ -33,17 +33,18 @@ grep -r "TESTING =" /etc/csf/csf.conf
 #Start changing  protocal
 echo "Start changing  protocal...."
 
-sed -i 's/TCP_IN = "20,21,22,25,53,80,110,143,443,465,587,993,995"/TCP_IN = ""/' /etc/csf/csf.conf
-sed -i 's/TCP_OUT = "20,21,22,25,53,80,110,113,443,587,993,995"/TCP_OUT = "80,443"/' /etc/csf/csf.conf
+sed -i 's/TCP_IN =.*/TCP_IN =""/' /etc/csf/csf.conf
+sed -i 's/TCP_OUT =.*/TCP_OUT ="80,443,53"/' /etc/csf/csf.conf
+sed -i 's/UDP_IN =.*/UDP_IN ="53"/' /etc/csf/csf.conf
+sed -i 's/UDP_OUT =.*/UDP_OUT ="53"/' /etc/csf/csf.conf
 
-sed -i 's/UDP_IN = "20,21,53,80,443"/UDP_IN = ""/' /etc/csf/csf.conf
-sed -i 's/UDP_OUT = "20,21,53,113,123"/UDP_OUT = ""/' /etc/csf/csf.conf
 
-sed -i 's/TCP6_IN = "20,21,22,25,53,80,110,143,443,465,587,993,995"/TCP6_IN = ""/' /etc/csf/csf.conf
-sed -i 's/TCP6_OUT = "20,21,22,25,53,80,110,113,443,587,993,995"/TCP6_OUT = ""/' /etc/csf/csf.conf
 
-sed -i 's/UDP6_IN = "20,21,53,80,443"/UDP6_IN = ""/' /etc/csf/csf.conf
-sed -i 's/UDP6_OUT = "20,21,53,113,123"/UDP6_OUT = ""/' /etc/csf/csf.conf
+sed -i 's/TCP6_IN =.*/TCP6_IN =""/' /etc/csf/csf.conf
+sed -i 's/TCP6_OUT =.*/TCP6_OUT =""/' /etc/csf/csf.conf
+sed -i 's/UDP6_IN =.*/UDP6_IN =""/' /etc/csf/csf.conf
+sed -i 's/UDP6_OUT .*/UDP6_OUT =""/' /etc/csf/csf.conf
+
 
 
 echo "Verify that the change was successful. ...."
