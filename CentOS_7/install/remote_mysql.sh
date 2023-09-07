71ec1c0cb50c5bea0bfe3dff4624104d71013f91
childgo <65421844+childgo@users.noreply.github.com>
2023-09-06 21:34:09 -0400

    Delete CentOS_7/install/remote_mysql.sh

--- CentOS_7/install/remote_mysql.sh
+++
@@ -1,6 +0,0 @@
-sestatus;sleep 3;
-getsebool -a | grep httpd;sleep 3;
-setsebool httpd_can_network_connect_db 1;sleep 3;
-setsebool -P httpd_can_network_connect_db 1;sleep 3;
-setsebool -P httpd_can_network_connect 1;sleep 3;
-getsebool -a | grep httpd_can_network_connect;sleep 3;
