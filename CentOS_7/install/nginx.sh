40609b102059d17d8d4af42fd4ccffc22331c056
childgo <65421844+childgo@users.noreply.github.com>
2023-09-06 21:12:49 -0400

    Create nginx.sh

---
+++ CentOS_7/install/nginx.sh
@@ -0,0 +1,13 @@
+yum update;sleep 3;
+yum -y update;sleep 3;
+yum install epel-release;sleep 3;
+yum install nginx;sleep 3;
+systemctl start nginx;sleep 3;
+systemctl enable nginx;sleep 3;
+systemctl status nginx;sleep 3;
+firewall-cmd --zone=public --permanent --add-service=http;sleep 3;
+firewall-cmd --zone=public --permanent --add-service=https;sleep 3;
+firewall-cmd --reload;sleep 3;
+systemctl reload nginx;sleep 3;
+systemctl restart nginx;sleep 3;
+reboot;sleep 3;
