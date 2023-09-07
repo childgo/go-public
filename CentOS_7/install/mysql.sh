b9bc7b9eee47a8d8d54faf68d4d7c740e39db721
childgo <65421844+childgo@users.noreply.github.com>
2023-09-06 21:12:22 -0400

    Create mysql.sh

---
+++ CentOS_7/install/mysql.sh
@@ -0,0 +1,22 @@
+
+#Part1
+yum -y install mariadb-server mariadb;sleep 3;
+systemctl start mariadb;sleep 3;
+systemctl enable mariadb;sleep 3;
+
+
+
+#Allow remote MySQL from php(to check remote database)
+sestatus;sleep 3;
+getsebool -a | grep httpd;sleep 3;
+setsebool httpd_can_network_connect_db 1;sleep 3;
+setsebool -P httpd_can_network_connect_db 1;sleep 3;
+setsebool -P httpd_can_network_connect 1;sleep 3;
+getsebool -a | grep httpd_can_network_connect;sleep 3;
+
+
+
+
+#checking
+#systemctl status mariadb;sleep 3;
+#mysql;sleep 3;
