014b757cf8af98b3de61d4e30957a56b3dce17d2
childgo <65421844+childgo@users.noreply.github.com>
2023-09-22 21:48:53 -0400

    Create Backup_MySQL.sh

---
+++ whm/Backup_MySQL.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Get the current date in the format "2023-9-22"
+date=$(date +%Y-%m-%d)
+
+# Define the backup directory path
+backup_root="/home/Backup_MySQL"
+backup_dir="$backup_root/$date"
+
+# Check if the backup directory exists, create it if not
+if [ ! -d "$backup_dir" ]; then
+    /usr/bin/mkdir -p -v "$backup_dir"
+else
+    echo "Backup directory $backup_dir already exists."
+fi
+
+# Loop through each database and create its backup
+for DB in $(mysql -Be "show databases" | /usr/bin/grep -v 'row\|information_schema\|Database'); do
+    echo "Generating MySQL backup of $DB"
+    
+    # Perform the database dump and save it as a compressed file
+    if /usr/bin/mysqldump --skip-lock-tables --events --routines --triggers ${DB} | /usr/bin/gzip -9 > "$backup_dir/${DB}.sql.gz"; then
+        echo "Backup of $DB completed."
+    else
+        echo "Backup of $DB failed."
+    fi
+done
+
+# Delete folders older than 30 days
+find "$backup_root" -type d -mtime +30 -exec rm -rf {} +
+
+# Print completion message
+echo "Complete."
