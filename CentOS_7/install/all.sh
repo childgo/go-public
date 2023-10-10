acf6a5a50a81c5cfca46f9cbd357b66323b246df
childgo <65421844+childgo@users.noreply.github.com>
2023-10-10 04:56:16 -0400

    Update all.sh

--- CentOS_7/install/all.sh
+++ CentOS_7/install/all.sh
@@ -1,4 +1,4 @@
-#bash <(curl -s https://raw.githubusercontent.com/childgo/go-public/master/CentOS_7/select.sh)
+#bash <(curl -s https://raw.githubusercontent.com/childgo/go-public/master/CentOS_7/install/all.sh)
 
 clear
 PS3='Please enter your choice: '
