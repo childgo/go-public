76ce352f4d37f1a5f4f7f21872e78a529762f293
childgo <65421844+childgo@users.noreply.github.com>
2024-02-25 03:24:54 -0500

    Create build_Repo.sh

---
+++ AlmaLinux9/build_Repo.sh
@@ -0,0 +1,5 @@
+
+createrepo /home/repolin/public_html/Linux/AlmaLinux9/AlmaLinux9_Sync_Repository/
+createrepo /home/repolin/public_html/Linux/AlmaLinux9/Nginx_SecureGateway
+chgrp -R repolin *
+chown -R repolin *
