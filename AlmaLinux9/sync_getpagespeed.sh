c76a0739de644b0c9cf9d6aadbb51be80cebe193
childgo <65421844+childgo@users.noreply.github.com>
2024-02-25 01:55:06 -0500

    Update sync_getpagespeed.sh1

--- AlmaLinux9/sync_getpagespeed.sh
+++ AlmaLinux9/sync_getpagespeed.sh
@@ -194,7 +194,7 @@ lsyncd
 #==============================================
 ##a loop to update repos one at a time
 for REPO in ${LOCAL_REPOS_Nginx}; do
-repotrack -a x86_64 -p /home/final/Linux/AlmaLinux9/Nginx_SecureGateway/$REPO $REPO
+repotrack -a x86_64 -p /home/repolin/public_html/Linux/AlmaLinux9/Nginx_SecureGateway/$REPO $REPO
 done
 #==============================================
 
@@ -204,6 +204,6 @@ done
 #==============================================
 ##a loop to update repos one at a time
 for REPO in ${LOCAL_REPOS_Tools}; do
-repotrack -a x86_64 -p /home/final/Linux/AlmaLinux9/Tools/$REPO $REPO
+repotrack -a x86_64 -p /home/repolin/public_html/Linux/AlmaLinux9/Nginx_SecureGateway/Tools/$REPO $REPO
 done
 #==============================================
