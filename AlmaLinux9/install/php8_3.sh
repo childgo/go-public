3847f98439d80bf6ad67d8e5a2f585b3e51773a3
childgo <65421844+childgo@users.noreply.github.com>
2024-03-01 21:19:41 -0500

    Update php8_3.sh

--- AlmaLinux9/install/php8_3.sh
+++ AlmaLinux9/install/php8_3.sh
@@ -3,7 +3,6 @@
 
 #dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm;sleep 3;
 #dnf install https://rpms.remirepo.net/enterprise/remi-release-9.rpm;sleep 3;
-dnf install https://rpms.remirepo.net/enterprise/remi-release-9.rpm
 
 dnf -y --nogpgcheck module list php;sleep 3;
 
