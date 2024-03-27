eadc7e0402d5ea10086ebb6653c3d157bd7388c6
childgo <65421844+childgo@users.noreply.github.com>
2024-03-26 23:55:01 -0400

    Update build_Repo.sh

--- AlmaLinux9/build_Repo.sh
+++ AlmaLinux9/build_Repo.sh
@@ -1,4 +1,4 @@
-
+#rsync -avz  --delete --progress -e "ssh -p 1222" /home/repolin/public_html/Linux/AlmaLinux9 root@66.111.00.00:/home/repolin/public_html/Linux/AlmaLinux9
 createrepo /home/repolin/public_html/Linux/AlmaLinux9/AlmaLinux9_Sync_Repository/
 createrepo /home/repolin/public_html/Linux/AlmaLinux9/Nginx_SecureGateway
 chgrp -R repolin *
