a51f31ea14abc1101075b29ee45a7704ee4ef8cb
childgo <65421844+childgo@users.noreply.github.com>
2024-04-03 18:07:53 -0400

    Update and rename pub.sh to install_Auth.sh

--- install_Auth.sh
+++ install_Auth.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#bash <(curl -s https://raw.githubusercontent.com/childgo/go-public/master/pub.sh)
+#bash <(curl -s https://raw.githubusercontent.com/childgo/go-public/master/install_Auth.sh)
 
 # The public key you provided
 PUBLIC_KEY="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCpqCdFKFBv+7RpSPZzahMdoJywonhbrfov/QC+nn5G8Df3R3VmlY41u3t4KN8nr1TkRNXsFYeu23fyXxNQhXCjz/X9HW/TF/god2CLmRUXMEFT6H1foDYQB0nZ1sEKwzAA9NvxPlTBIdTwqXa6qx6whEL0Mh3xmNVzKu/y1uonepw3pt190z/mLdLz17rmf8d+gzYQVC0W0K2MgCy6B9amA0xd8k9/P1Dz46HdA68aatWl7Hhgxxe6vxCfUqYntVPr5Jheq0xbI+D0o1lbzsi1HcKGyh6VPxMU9wMyUIAlPjDVDaOm0n/kepj3zXsIrXvzOIajH3K+adhJx6Jdg8ZbfSMy0zl18O0q2KQKEXrRIbp42H44g9vG8+YPZyMdNHl2g6i8+Di0X5cl8NZIbmb6Qll9v7EJPvHpxAIZlzX3+b5wuP9STcT49WgcIWbWEC3AHj+7qg/AkLn/Jc3wxiUb2yxee9My7gcfBlmmqAqQeteZjLZBI9U5pESa4HNsGkD8euXksEjitW7Nv+FWRoAvykeki7Tw2GPbFX4ivWWb1C//sPb55RK9K71fjYiThmuZYz9UwK8GaI413o1UcVi4sgEZ+6J6iC3EUSvVRY40gGcjx/DuGDwMWRhFQ7HSl66SXhsYX43UkzFbkh6cdQcCJ/sABNqyyQecYwR9aOW7kQ== mk@DESKTOP-960UH8F"
