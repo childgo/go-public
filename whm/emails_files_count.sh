987c92b9043535bdae7d741ca18a21aa27885781
childgo <65421844+childgo@users.noreply.github.com>
2024-06-17 12:36:20 -0400

    Update emails_files_count.sh

--- whm/emails_files_count.sh
+++ whm/emails_files_count.sh
@@ -24,7 +24,6 @@ case $group_choice in
         count_files "/home/soc"
         count_files "/home/gcans"
         count_files "/home/icaagov"
-        count_files "/home/industry"
         count_files "/home/motgov"
         ;;
     2)
@@ -45,6 +44,7 @@ case $group_choice in
         count_files "/home/tocoil"
         count_files "/home/nrcoil"
         count_files "/home/mowr"
+        count_files "/home/industry"
 
         echo -e "\n\n\n\n"
         ;;
@@ -54,30 +54,36 @@ case $group_choice in
     5)
         echo "User selected Group5_101_62 [00.00.101.62]"
         echo "Part5 [00.00.101.62]"
+        count_files "/home/mod"
+        count_files "/home/mcbpcgov"
+        count_files "/home/oecoil"
+        count_files "/home/ifargov"
         count_files "/home/bsroti"
-        count_files "/home/fbsamail"
-        count_files "/home/gfcoil"
+        count_files "/home/oilprdc"
+        count_files "/home/tocoil"
+        count_files "/home/nanepedu"
         count_files "/home/heesco"
         count_files "/home/idcgov"
-        count_files "/home/ifargov"
-        count_files "/home/iotcoil"
-        count_files "/home/kotioil"
-        count_files "/home/mcbpcgov"
-        count_files "/home/mdocoil"
-        count_files "/home/mod"
         count_files "/home/mrcoil"
-        count_files "/home/nanepedu"
-        count_files "/home/ngcoil"
-        count_files "/home/oecoil"
-        count_files "/home/opcoil"
-        count_files "/home/opdcoil"
-        count_files "/home/otibaiji"
-        count_files "/home/oilprdc"
-        count_files "/home/rafidain"
         count_files "/home/research"
+        count_files "/home/fbsamail"
         count_files "/home/sgcoil"
-        count_files "/home/tocoil"
+        count_files "/home/ngcoil"
+        count_files "/home/mdocoil"
+        count_files "/home/gfcoil"
+        count_files "/home/opdcoil"
+        count_files "/home/iotcoil"
+        count_files "/home/rafidain"
+        count_files "/home/otibaiji"
+        count_files "/home/opcoil"
+        count_files "/home/kotioil"
         count_files "/home/motioil"
+        count_files "/home/ismamo"
+        count_files "/home/council"
+
+
+
+
         echo -e "\n\n\n\n"
         # Add additional directories for Group4 here if needed
         ;;
