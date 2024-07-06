c1ee3ec6e75e405a866efc0ed30fb18721b88297
childgo <65421844+childgo@users.noreply.github.com>
2024-07-05 22:01:58 -0400

    Update sync_php73.sh

--- CentOS_7/sync_php73.sh
+++ CentOS_7/sync_php73.sh
@@ -1,3 +1,39 @@
 #!/bin/bash
 #bash <(curl -s https://raw.githubusercontent.com/childgo/go-public/master/CentOS_7/sync_php73.sh)
-sudo dnf install dnf-plugins-core createrepo_c -y
+clear
+
+
+
+# Download specified tools
+LOCAL_REPOS_PHP73_Tools="
+mysqltuner-cron
+mysqltuner
+ngxtop
+remote_syslog2
+fail2ban
+closure-compiler
+policycoreutils-python
+setroubleshoot-server
+selinux-policy-doc
+attr
+maldet
+fcgiwrap
+conntrack-tools
+fds
+closure-compiler
+cloud-utils-growpart
+mutt
+zip
+epel-release
+yum-utils
+lsyncd
+"
+
+# Create directory for downloaded tools if it doesn't exist
+download_dir="/home/final/php73"
+mkdir -p "$download_dir"
+
+# Download the specified tools
+for REPO in ${LOCAL_REPOS_PHP73_Tools}; do
+    repotrack -a x86_64 -p "$download_dir" "$REPO"
+done
