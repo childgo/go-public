e3f1064b72532111d568ea5cc2453d7e2946eef4
childgo <65421844+childgo@users.noreply.github.com>
2024-07-06 12:53:26 -0400

    Update all.sh

--- AlmaLinux9/install/all.sh
+++ AlmaLinux9/install/all.sh
@@ -89,7 +89,7 @@ dnf clean all
 systemctl disable firewalld;sleep 3;
 systemctl stop firewalld;sleep 3;
 dnf clean all;sleep 3;
-dnf install -y update;sleep 3;
+dnf -y update;sleep 3;
 dnf install -y install wget nano inotify-tools rsync sshpass iftop htop createrepo repotrack;sleep 3;
 dnf install -y install perl tar ipset unzip net-tools perl-libwww-perl;sleep 3;
 dnf install -y install perl-LWP-Protocol-https perl-GDGraph bind-utils;sleep 3;
