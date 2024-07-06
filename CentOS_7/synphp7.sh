2068fcc43165c70eb35746c1cb4496115ae7b243
childgo <65421844+childgo@users.noreply.github.com>
2024-07-05 23:26:05 -0400

    Create synphp7.sh

---
+++ CentOS_7/synphp7.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# Download remi-release-7.rpm to the script directory
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+REMIREPO_RPM="$SCRIPT_DIR/remi-release-7.rpm"
+curl -o "$REMIREPO_RPM" https://rpms.remirepo.net/enterprise/remi-release-7.rpm
+
+# Install remi-release-7.rpm
+sudo yum -y install "$REMIREPO_RPM"
+
+# Download specified PHP tools
+LOCAL_REPOS_PHP73_Tools="
+    epel-release
+    yum-utils
+    php
+    php-cli
+    php-fpm
+    php-mysqlnd
+    php-zip
+    php-devel
+    php-gd
+    php-mbstring
+    php-curl
+    php-xml
+    php-pear
+    php-bcmath
+    php-json
+"
+
+# Create directory for downloaded tools if it doesn't exist
+download_dir="/home/final/php73"
+mkdir -p "$download_dir"
+
+# Download the specified tools
+for REPO in ${LOCAL_REPOS_PHP73_Tools}; do
+    repotrack -a x86_64 -p "$download_dir" "$REPO"
+done
