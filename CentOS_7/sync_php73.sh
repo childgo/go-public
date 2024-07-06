0097a99e82c87069c4d54472a48a81ec3c73e4f0
childgo <65421844+childgo@users.noreply.github.com>
2024-07-05 21:59:06 -0400

    Update sync_php73.sh

--- CentOS_7/sync_php73.sh
+++ CentOS_7/sync_php73.sh
@@ -1,2 +1,3 @@
-
-#https://raw.githubusercontent.com/childgo/go-public/master/CentOS_7/sync_php73.sh
+#!/bin/bash
+#bash <(curl -s https://raw.githubusercontent.com/childgo/go-public/master/CentOS_7/sync_php73.sh)
+sudo dnf install dnf-plugins-core createrepo_c -y
