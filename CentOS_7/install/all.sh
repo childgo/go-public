0c1404a80b964eb0a6dfa1cdd4c4e8b4591443fc
childgo <65421844+childgo@users.noreply.github.com>
2024-07-06 13:23:19 -0400

    Update all.sh

--- CentOS_7/install/all.sh
+++ CentOS_7/install/all.sh
@@ -87,7 +87,7 @@ systemctl disable firewalld;sleep 3;
 systemctl stop firewalld;sleep 3;
 yum clean all;sleep 3;
 yum -y update;sleep 3;
-yum -y install wget nano inotify-tools rsync sshpass iftop htop createrepo repotrack;sleep 3;
+yum -y install wget nano inotify-tools rsync sshpass iftop htop createrepo;sleep 3;
 yum -y install perl tar ipset unzip net-tools perl-libwww-perl;sleep 3;
 yum -y install perl-LWP-Protocol-https perl-GDGraph bind-utils;sleep 3;
 cd /opt;sleep 3;
