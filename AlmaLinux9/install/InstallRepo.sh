733a963863f529b8eaca754ebd11d0ec000dd32c
childgo <65421844+childgo@users.noreply.github.com>
2024-07-17 20:07:39 -0400

    Update InstallRepo.sh

--- AlmaLinux9/install/InstallRepo.sh
+++ AlmaLinux9/install/InstallRepo.sh
@@ -3,55 +3,119 @@
 # Delete all files in /etc/yum.repos.d/
 rm -f /etc/yum.repos.d/*
 
-# Create new repo files with the embedded content
+
+
 cat <<EOF > /etc/yum.repos.d/ALSCO_SecureGateway_AlmaLinux9.repo
 # Content from ALSCO_SecureGateway_AlmaLinux9.repo
-[AlmaLinux9-base]
-name=AlmaLinux 9 - Base
-baseurl=http://repo.example.com/almalinux/9/base
+[ALSCO_Secure_Gateway-baseos]
+name=ALSCO_Secure_Gateway-BaseOS
+baseurl=https://repo.alscoip.com/Linux/AlmaLinux9/AlmaLinux9_Sync_Repository/baseos/
 enabled=1
-gpgcheck=1
-gpgkey=http://repo.example.com/RPM-GPG-KEY-AlmaLinux
+gpgcheck=0
+
+[ALSCO_Secure_Gateway-appstream]
+name=ALSCO_Secure_Gateway-AppStream
+baseurl=https://repo.alscoip.com/Linux/AlmaLinux9/AlmaLinux9_Sync_Repository/appstream/
+enabled=1
+gpgcheck=0
+
+[ALSCO_Secure_Gateway-extras]
+name=ALSCO_Secure_Gateway-Extras
+baseurl=https://repo.alscoip.com/Linux/AlmaLinux9/AlmaLinux9_Sync_Repository/extras/
+enabled=1
+gpgcheck=0
+
+[ALSCO_Secure_Gateway-crb]
+name=ALSCO_Secure_Gateway-CRB
+baseurl=https://repo.alscoip.com/Linux/AlmaLinux9/AlmaLinux9_Sync_Repository/crb/
+enabled=1
+gpgcheck=0
+#Finish Content from ALSCO_SecureGateway_AlmaLinux9.repo
 EOF
+
+
+
+
+
+
+
 
 cat <<EOF > /etc/yum.repos.d/ALSCO_SecureGateway_Module.repo
 # Content from ALSCO_SecureGateway_Module.repo
-[Module-Stream]
-name=Module Stream
-baseurl=http://repo.example.com/module/stream
+[ALSCO_Secure_Gateway_ModulesSpeed-extras]
+name=ALSCO_Secure_Gateway Modules extras for Enterprise
+baseurl=https://repo.alscoip.com/Linux/AlmaLinux9/SecureGateway_Module/getpagespeed-extras/
 enabled=1
-gpgcheck=1
-gpgkey=http://repo.example.com/RPM-GPG-KEY-ModuleStream
+gpgcheck=0
+repo_gpgcheck=0
+module_hotfixes=1
+priority=9
+
+[ALSCO_Secure_Gateway_ModulesSpeed-noarch]
+name=ALSCO_Secure_Gateway Modules noarch for Enterprise
+baseurl=https://repo.alscoip.com/Linux/AlmaLinux9/SecureGateway_Module/getpagespeed-extras-noarch/
+enabled=1
+gpgcheck=0
+repo_gpgcheck=0
+module_hotfixes=1
+priority=9
+#Finish Content from ALSCO_SecureGateway_Module.repo
 EOF
+
+
+
+
+
+
+
 
 cat <<EOF > /etc/yum.repos.d/ALSCO_SecureGateway_Others_Requires.repo
 # Content from ALSCO_SecureGateway_Others_Requires.repo
-[Others-Requires]
-name=Others Requires
-baseurl=http://repo.example.com/others/requires
+[ALSCO-Secure_Gateway_Others_Requires]
+name=ALSCO-Secure_Gateway_Others_Requires
+baseurl=https://repo.alscoip.com/Linux/AlmaLinux9/SecureGateway_Others_Requires/
 enabled=1
-gpgcheck=1
-gpgkey=http://repo.example.com/RPM-GPG-KEY-OthersRequires
+gpgcheck=0
+#Finish Content from ALSCO_SecureGateway_Others_Requires.repo
 EOF
+
+
+
+
+
+
+
+
+
+
 
 cat <<EOF > /etc/yum.repos.d/ALSCO_SecureGateway_php83.repo
 # Content from ALSCO_SecureGateway_php83.repo
-[PHP-8.3]
-name=PHP 8.3 Repository
-baseurl=http://repo.example.com/php/8.3
+[ALSCO_Secure_Gateway-php8.3]
+name=ALSCO_Secure_Gateway-php8.3
+baseurl=https://repo.alscoip.com/Linux/AlmaLinux9/Secure_Gateway_php83/
 enabled=1
-gpgcheck=1
-gpgkey=http://repo.example.com/RPM-GPG-KEY-PHP83
+gpgcheck=0
+#Finish Content from ALSCO_SecureGateway_php83.repo
 EOF
+
+
+
+
+
+
+
+
+
 
 cat <<EOF > /etc/yum.repos.d/ALSCO_SecureGateway_Tools.repo
 # Content from ALSCO_SecureGateway_Tools.repo
-[Tools]
-name=Tools Repository
-baseurl=http://repo.example.com/tools
+[ALSCO_Secure_Gateway_Tools]
+name=ALSCO_Secure_Gateway_Tools
+baseurl=https://repo.alscoip.com/Linux/AlmaLinux9/SecureGateway_Tools/
 enabled=1
-gpgcheck=1
-gpgkey=http://repo.example.com/RPM-GPG-KEY-Tools
+gpgcheck=0
+#Finish Content from ALSCO_SecureGateway_Tools.repo
 EOF
 
 echo "Repository files have been created with the updated content."
