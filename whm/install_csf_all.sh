ac802b01ae335175b1065ea03f5a596957e7fd23
childgo <65421844+childgo@users.noreply.github.com>
2025-04-24 13:07:29 -0400

    Update install_csf_all.sh

--- whm/install_csf_all.sh
+++ whm/install_csf_all.sh
@@ -17,25 +17,62 @@ clear
 #Installation CSF [ConfigServer Security and Firewall]
 #======================================================================
 echo "Installtion [ConfigServer Security and Firewall]"
-cd /usr/src;sleep 3;
-rm -fv csf.tgz;sleep 3;
+
+
+
+
+
+
+
+systemctl disable firewalld;sleep 3;
+systemctl stop firewalld;sleep 3;
+dnf clean all;sleep 3;
+dnf -y update;sleep 3;
+dnf -y install wget nano inotify-tools rsync sshpass iftop htop createrepo traceroute;sleep 3;
+dnf -y install perl tar ipset tcpdump unzip net-tools perl-libwww-perl;sleep 3;
+dnf -y install perl-LWP-Protocol-https perl-GDGraph bind-utils;sleep 3;
+dnf -y install epel-release createrepo;sleep 3;
+dnf -y install bind-utils -y;sleep 3;
+dnf -y install nc -y;sleep 3;
+dnf -y install nmap-ncat -y;sleep 3;
+
+
+cd /opt;sleep 3;
 wget https://download.configserver.com/csf.tgz;sleep 3;
 tar -xzf csf.tgz;sleep 3;
 cd csf;sleep 3;
 sh install.sh;sleep 3;
+rm -rf /opt/csf;sleep 3;
+rm -rf /opt/csf.tgz;sleep 3;
+cd ~;sleep 3;
 
-#Enable CSF
+systemctl enable csf;sleep 3;
+systemctl enable lfd;sleep 3;
+#service csf start;sleep 3;
+#service lfd start;sleep 3;
+#perl /usr/local/csf/bin/csftest.pl;sleep 3;
+
+
+
+#======================================================================
+#CSF Setting
+#======================================================================
 echo "Start changing CSF from '1' to '0' to activate it...."
 sed -i 's/TESTING = "1"/TESTING = "0"/' /etc/csf/csf.conf
 echo "Verify that the change was successful. ...."
 grep -r "TESTING =" /etc/csf/csf.conf
 
 
-#Enable IPSET [Change LF_IPSET from 0 to 1]
-echo "Change LF_IPSET from 0 to 1, becuase 1 mean enable"
-sed -i 's/^LF_IPSET =.*/LF_IPSET = "1"/' /etc/csf/csf.conf
-grep -r "LF_IPSET =" /etc/csf/csf.conf
-echo ""
+
+#Start changing  protocal
+echo "Start changing  protocal...."
+
+sed -i 's/TCP_IN =.*/TCP_IN =""/' /etc/csf/csf.conf
+sed -i 's/TCP_OUT =.*/TCP_OUT ="80,443,53"/' /etc/csf/csf.conf
+sed -i 's/UDP_IN =.*/UDP_IN ="53"/' /etc/csf/csf.conf
+sed -i 's/UDP_OUT =.*/UDP_OUT ="53"/' /etc/csf/csf.conf
+
+
 
 sed -i 's/TCP6_IN =.*/TCP6_IN =""/' /etc/csf/csf.conf
 sed -i 's/TCP6_OUT =.*/TCP6_OUT =""/' /etc/csf/csf.conf
@@ -44,15 +81,52 @@ sed -i 's/UDP6_OUT .*/UDP6_OUT =""/' /etc/csf/csf.conf
 
 
 
-#Start CSF
-systemctl enable csf;sleep 3;
-systemctl enable lfd;sleep 3;
-service csf start;sleep 3;
-service lfd start;sleep 3;
-perl /usr/local/csf/bin/csftest.pl;sleep 3;
+#Enable IPSET [Change LF_IPSET from 0 to 1]
+echo "Change LF_IPSET from 0 to 1, becuase 1 mean enable"
+sed -i 's/^LF_IPSET =.*/LF_IPSET = "1"/' /etc/csf/csf.conf
+grep -r "LF_IPSET =" /etc/csf/csf.conf
+echo ""
+echo ""
+
+echo "Verify that the change was successful. ...."
+grep -r "TCP_IN =" /etc/csf/csf.conf
+grep -r "TCP_OUT =" /etc/csf/csf.conf
+grep -r "UDP_IN =" /etc/csf/csf.conf
+grep -r "UDP_OUT =" /etc/csf/csf.conf
+echo ""
+
+grep -r "TCP6_IN =" /etc/csf/csf.conf
+grep -r "TCP6_OUT =" /etc/csf/csf.conf
+grep -r "UDP6_IN =" /etc/csf/csf.conf
+grep -r "UDP6_OUT =" /etc/csf/csf.conf
+
+echo "Change LF_IPSET from 0 to 1, becuase 1 mean enable"
+grep -r "LF_IPSET =" /etc/csf/csf.conf
+
+
+echo ""
 #======================================================================
-#CSF End
+
+
+systemctl enable csf
+systemctl enable lfd
+service csf start
+service lfd start
+perl /usr/local/csf/bin/csftest.pl
+
+
+
 #======================================================================
+#Uninstall CSF Firewall
+#cd /etc/csf
+#sh uninstall.sh
+
+
+
+
+
+
+
 
 
 
