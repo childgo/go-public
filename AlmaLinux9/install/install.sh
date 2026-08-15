09cfdb115b3eec4181360eb8f6a976f9b5dd4457
childgo <65421844+childgo@users.noreply.github.com>
2026-08-15 13:45:45 -0400

    Add port 587 to TCP_OUT in csf.conf

--- AlmaLinux9/install/install.sh
+++ AlmaLinux9/install/install.sh
@@ -357,7 +357,7 @@ grep -r "TESTING =" /etc/csf/csf.conf
 echo "Start changing  protocal...."
 
 sed -i 's/TCP_IN =.*/TCP_IN =""/' /etc/csf/csf.conf
-sed -i 's/TCP_OUT =.*/TCP_OUT ="80,443,53"/' /etc/csf/csf.conf
+sed -i 's/TCP_OUT =.*/TCP_OUT ="80,443,53,587"/' /etc/csf/csf.conf
 sed -i 's/UDP_IN =.*/UDP_IN ="53"/' /etc/csf/csf.conf
 sed -i 's/UDP_OUT =.*/UDP_OUT ="53"/' /etc/csf/csf.conf
 
